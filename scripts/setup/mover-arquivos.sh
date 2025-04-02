1cf54cbd006dc836b09890a43206f6e9ebd1a174
Marcello Lee <marcellohslee@gmail.com>
2025-04-02 15:24:05 -0300

    Delete scripts/setup directory

--- scripts/setup/mover-arquivos.sh
+++
@@ -1,7 +0,0 @@
-
-#!/bin/bash
-
-
-sudo -S docker cp scripts/CreateTarget/create-targets-from-host-list.gmp.py e38b91a3fb5a:/auto_vas/create-targets-from-host-list.gmp.py
-sudo -S docker cp scripts/CreateTask/create-tasks-from-csv.gmp.py e38b91a3fb5a:/auto_vas/create-tasks-from-csv.gmp.py
-sudo -S docker cp scripts/RunScan/start-scans-from-csv.py e38b91a3fb5a:/auto_vas/start-scans-from-csv.py
